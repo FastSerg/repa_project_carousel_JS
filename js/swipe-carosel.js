d717e531d9a741e162e95bcebf776832bb822ca1
Fastovec <sergejfastovec284@gmail.com>
2022-07-07 15:39:27 +0300

    fixed bugs

--- js/swipe-carosel.js
+++ js/swipe-carosel.js
@@ -1,22 +1,22 @@
 
-function SwipeCarosel() {
+function SwipeCarousel() {
   Carousel.apply(this, arguments);
 }
 
-SwipeCarosel.prototype = Object.create(Carousel.prototype);
-SwipeCarosel.prototype.constructor = SwipeCarosel;
+SwipeCarousel.prototype = Object.create(Carousel.prototype);
+SwipeCarousel.prototype.constructor = SwipeCarousel;
 
-SwipeCarosel.prototype._initListeners = function() {
+SwipeCarousel.prototype._initListeners = function() {
   Carousel.prototype._initListeners.apply(this);
   this.container.addEventListener('touchstart', this._swipeStart.bind(this));
   this.container.addEventListener('touchend', this._swipeEnd.bind(this));
 }
 
-SwipeCarosel.prototype._swipeStart = function(event) {
+SwipeCarousel.prototype._swipeStart = function(event) {
   this.swipeStartX = event.changedTouches[0].pageX;
 };
 
-SwipeCarosel.prototype._swipeEnd = function(event) {
+SwipeCarousel.prototype._swipeEnd = function(event) {
   this.swipeEndX = event.changedTouches[0].pageX;
   this.swipeStartX - this.swipeEndX < -100 && this.prev();
   this.swipeStartX - this.swipeEndX > 100 && this.next();
